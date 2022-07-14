2e05f8357a967de9dcab0edb7116d0e8e4238bcc
SiaDeLong <ericsiadelong@outlook.com>
2022-07-14 22:46:46 +0800

    this is the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,54 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import { Container, Row, Col } from 'react-grid-system';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Matrixtudios</title>
+        <link rel="icon" href="/Matrixtudios_Logo.ico" />
       </Head>
+
+      <header>
+        <Container fluid>
+          <Row>
+            <Col sm={7}>
+              <Image src={"/Matrixtudios_Logo.png"} width= '152px' height= '100px'/>
+            </Col>
+            <Col sm={1}>
+              <br></br>
+              <br></br>
+              Home
+            </Col>
+            <Col sm={1}>
+              <br></br>
+              <br></br>
+              Services
+            </Col>
+            <Col sm={1}>
+              <br></br>
+              <br></br>
+              About
+            </Col>
+            <Col sm={2}>
+              <br></br>
+              <br></br>
+              Contact Us
+            </Col>
+          </Row>
+        </Container>
+      </header>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Matrixtudios
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Provide you a whole new experience of virtual reality<br />
+          and augmented reality to your life.
         </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer>
@@ -60,13 +63,20 @@ export default function Home() {
       </footer>
 
       <style jsx>{`
+        header {
+          margin-top: 1.0rem;
+        }
+
+        .headerItem{
+          margin-top: 10rem;
+        }
+
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
+          
         }
 
         main {
@@ -74,8 +84,7 @@ export default function Home() {
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
+
         }
 
         footer {
@@ -97,36 +106,24 @@ export default function Home() {
           align-items: center;
         }
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
         .title {
-          margin: 0;
+          margin-top: 5.0rem;
+          margin-left: 1.5rem;
+          margin-bottom: 0rem;
           line-height: 1.15;
           font-size: 4rem;
         }
 
         .title,
         .description {
-          text-align: center;
+          text-align: left;
         }
 
         .description {
+          margin-top: 0.5rem;
+          margin-left: 1.5rem;
           line-height: 1.5;
-          font-size: 1.5rem;
+          font-size: 1.25rem;
         }
 
         code {
@@ -136,46 +133,6 @@ export default function Home() {
           font-size: 1.1rem;
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
         }
 
         .logo {
